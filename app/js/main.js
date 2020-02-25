d274ea37f64688a634eb64316ad8f6e0e1db2cf3
Demi24 <appega@mail.ru>
2020-02-25 20:38:13 +0400

    added slider Your Followers Feed

--- app/js/main.js
+++ app/js/main.js
@@ -1,13 +1,22 @@
-$(function(){
+$(function () {
 
-$('.product__block-slider').slick({
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/icons/chevron-left.png" alt=""></button>',
-    nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/icons/chevron-right.png" alt=""></button>',
-    appendArrows: '.weekly__products-btn'
-});
+    $('.product__block-slider').slick({
+        infinite: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/icons/chevron-left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/icons/chevron-right.png" alt=""></button>',
+        appendArrows: '.weekly__products-btn'
+    });
 
-  
+    $('.release__item-slider').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="img/icons/chevron-left.png" alt=""></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="img/icons/chevron-right.png" alt=""></button>',
+        appendArrows: '.followers-btn'
+    });
+
+
 });
